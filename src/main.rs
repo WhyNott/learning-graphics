543dda49fe203775772c243753d9b606fd628186
Jan Wanot <Jan.Wanot.2017@live.rhul.ac.uk>
2021-07-16 20:49:02 +0100

    Implmeneted line drawing

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,6 @@ type Color = u32;
 
 use math::{Vector3};
 
-struct Sphere {
-    center: Vector3,
-    radius: f64,
-    color: Color
-}
-
-struct Scene {
-    viewport_size: (f64, f64),
-    projection_plane_d: f64,
-    spheres: Vec<Sphere>
-}
-
-
-
 const fn from_u8_rgb(r: u8, g: u8, b: u8) -> Color {
     let (r, g, b) = (r as u32, g as u32, b as u32);
     (r << 16) | (g << 8) | b
@@ -41,95 +27,68 @@ fn putpixel(buffer: &mut Vec<Color>, x: isize, y:isize, color: Color){
     }
 }
 
-
-fn canvas_to_viewport(scene: &Scene, x: isize, y: isize) -> Vector3 {
-    let (x, y) = (x as f64, y as f64);
-    let (v_w, v_h) = scene.viewport_size;
-    return [x*v_w/(WIDTH as f64), y*v_h/(HEIGHT as f64), scene.projection_plane_d];
-}
-use math::{vec3_sub, vec3_dot};
-
-fn intersect_ray_sphere(O: Vector3, D: Vector3,  sphere: &Sphere) -> (f64, f64) {
-    let r = sphere.radius;
-    let CO = vec3_sub(O, sphere.center);
-
-    let a = vec3_dot(D, D);
-    let b = 2.0*vec3_dot(CO, D);
-    let c = vec3_dot(CO, CO) - r*r;
-
-    let discriminant = b*b - 4.0*a*c;
-
-    if discriminant < 0.0 {
-        return (f64::INFINITY, f64::INFINITY);
+fn interpolate(i0:isize, d0:isize, i1:isize, d1:isize) -> Vec<f64> {
+    if i0 == i1 {
+        return vec![d0 as f64];
     }
-
-    let t1 = (-b + discriminant.sqrt()) / (2.0*a);
-    let t2 = (-b - discriminant.sqrt()) / (2.0*a);
-    return (t1, t2);    
+    let mut values = Vec::new();
+    let a = (d1 - d0) as f64 / (i1 - i0) as f64;
+    let mut d = d0 as f64;
+    for _i in i0..i1 {
+        values.push(d);
+        d = d + a;
+    }
+    return values;
 }
 
-fn trace_ray(scene: &Scene, O: Vector3, D: Vector3, t_min: f64, t_max: f64) -> Color{
-    let mut closest_t = f64::INFINITY;
-    let mut closest_sphere : Option<&Sphere> = None;
-    for sphere in &scene.spheres {
-        let (t1, t2) = intersect_ray_sphere(O, D, sphere);
-        if t1 > t_min && t1 < t_max && t1 < closest_t {
-            closest_t = t1;
-            closest_sphere = Some(&sphere);
-        }
-        
-        if t2 > t_min && t2 < t_max && t2 < closest_t {
-            closest_t = t2;
-            closest_sphere = Some(&sphere);
+fn draw_line(buffer: &mut Vec<Color>, p0: (isize, isize), p1: (isize, isize),  color: Color){
+    let (x0, x1, y0, y1) : (isize, isize, isize, isize);
+    
+    if (p1.0-p0.0).abs() > (p1.1-p0.1).abs() {
+        if p0.0 > p1.0 {
+            x0 = p1.0;
+            y0 = p1.1;
+            x1 = p0.0;
+            y1 = p0.1;
+        } else {
+            x0 = p0.0;
+            y0 = p0.1;
+            x1 = p1.0;
+            y1 = p1.1;
         }
 
-    }
-
-    if let Some(sphere) = closest_sphere {
-        return sphere.color;
+        let ys = interpolate(x0, y0, x1, y1);
+        for x in x0..x1{
+            putpixel(buffer, x, ys[(x - x0) as usize] as isize, color);
+        }
     } else {
-        return BACKGROUND_COLOR;
+        if p0.1 > p1.1 {
+            x0 = p1.0;
+            y0 = p1.1;
+            x1 = p0.0;
+            y1 = p0.1;
+        } else {
+            x0 = p0.0;
+            y0 = p0.1;
+            x1 = p1.0;
+            y1 = p1.1;
+        }
+
+        let xs = interpolate(y0, x0, y1, x1);
+        for y in y0..y1{
+            putpixel(buffer, xs[(y - y0) as usize] as isize, y, color);
+        }
+
     }
 }
-
 
 fn main() {
-    let scene = Scene {
-        viewport_size: (1.0, 1.0),
-        projection_plane_d: 1.0,
-        spheres: vec![
-            Sphere {
-                center: [0.0, -1.0, 3.0],
-                radius: 1.0,
-                color: from_u8_rgb(255, 0, 0)
-            },
-            Sphere {
-                center: [2.0, 0.0, 4.0],
-                radius: 1.0,
-                color: from_u8_rgb(0, 0, 255)
-            },
-            Sphere {
-                center: [-2.0, 0.0, 4.0],
-                radius: 1.0,
-                color: from_u8_rgb(0, 255, 0)
-            },
-        ]
-    };
-
-
     
     let mut buffer: Vec<Color> = vec![ BACKGROUND_COLOR; WIDTH * HEIGHT];
-    //putpixel(&mut buffer, 0, 0, from_u8_rgb(0, 255, 0));
-    let O: Vector3 = [0.0, 0.0, 0.0];
-    for x in -(WIDTH as isize)/2..(WIDTH as isize)/2 {
-        for y in -(HEIGHT as isize)/2..(HEIGHT as isize)/2 {
-            let D = canvas_to_viewport(&scene, x, y);
-            let color = trace_ray(&scene, O, D, 1.0, f64::INFINITY);
-            putpixel(&mut buffer, x, y, color);
-        }
-    }
-
-
+  
+    draw_line(&mut buffer, (-200, -100), (240, 120), from_u8_rgb(0, 0, 0));
+    
+    draw_line(&mut buffer, (-50, -200), (60, 240), from_u8_rgb(0, 0, 0));
     
     let mut window = Window::new(
         "Test - ESC to exit",
